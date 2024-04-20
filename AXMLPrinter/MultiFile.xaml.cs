ddba914868a230a828494270bb8e999468b7ba30
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-04-20 21:05:50 +0800

    多文件页面选择框修改

--- AXMLPrinter/MultiFile.xaml.cs
+++ AXMLPrinter/MultiFile.xaml.cs
@@ -31,11 +31,11 @@ namespace AXMLPrinter
 
                 if (string.IsNullOrEmpty(InputFolderPath.Text))
                 {
-                    LogBox.Text = Log.Add(LogBox.Text, "您已添加文件夹路径，路径为：" + path);
+                    LogBox.Text = Log.Add(LogBox.Text, "您已添加输入文件夹路径，路径为：" + path);
                 }
                 else
                 {
-                    LogBox.Text = Log.Add(LogBox.Text, "您已更新文件夹路径，路径为：" + path);
+                    LogBox.Text = Log.Add(LogBox.Text, "您已更新输入文件夹路径，路径为：" + path);
                 }
 
                 InputFolderPath.Text = path;
@@ -60,12 +60,44 @@ namespace AXMLPrinter
             if (result == true)
             {
                 string path = openFolderDialog.FolderName;
+
+                if (string.IsNullOrEmpty(OutputFolderPath.Text))
+                {
+                    LogBox.Text = Log.Add(LogBox.Text, "您已添加输出文件夹路径，路径为：" + path);
+                }
+                else
+                {
+                    LogBox.Text = Log.Add(LogBox.Text, "您已更新输出文件夹路径，路径为：" + path);
+                }
+
                 OutputFolderPath.Text = path;
             }
             else
             {
                 LogBox.Text = Log.Add(LogBox.Text, "选择输出文件夹出错");
             }
+        }
+
+        private void RunBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(UseAsyncCheckBox.IsChecked == true)
+            {
+                LogBox.Text = Log.Add(LogBox.Text, "使用异步模式(Beta)运行");
+            }
+            else
+            {
+                LogBox.Text = Log.Add(LogBox.Text, "使用同步模式运行");
+            }
+        }
+
+        private void UseAsyncCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            LogBox.Text = Log.Add(LogBox.Text, "已取消异步模式(Beta)");
+        }
+
+        private void UseAsyncCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            LogBox.Text = Log.Add(LogBox.Text, "已选择异步模式(Beta)");
         }
     }
 }
